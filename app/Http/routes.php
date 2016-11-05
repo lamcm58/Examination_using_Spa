595013382dd88052774e6625916d4897c24ad6e9
Cao Minh Lam <caominhlam30395@gmail.com>
2016-11-05 17:22:51 +0700

    update file route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,10 +17,27 @@ Route::get('/', function () {
 
 //-------------------- Admin zone ----------------------//
 Route::group(['prefix' => 'admin'], function(){
+	Route::get('', function(){
+		return view('admin');
+	});
 	Route::group(['prefix' => 'monhoc'], function(){
-		Route::get('list', [
-			'as' => 'admin.monhoc.list', 
-			'uses' => 'MonHocController@getList'
-		]);
+		Route::get('list', ['uses' => 'MonHocController@getList']);
+		Route::post('add', ['uses' => 'MonHocController@getAdd']);
+		Route::get('edit/{id_monthi}', ['uses' => 'MonHocController@getEdit']);
+		Route::post('edit/{id_monthi}', ['uses' => 'MonHocController@postEdit']);
+		Route::get('delete/{id_monthi}', ['uses' => 'MonHocController@getDelete']);
+	});
+	Route::group(['prefix' => 'cauhoi'], function(){
+		Route::get('', function(){
+			return view('admin.cauhoi');
+		});
+		Route::get('list', ['uses' => 'CauHoiController@getList']);
+		Route::post('add', ['uses' => 'CauHoiController@getAdd']);
+		Route::get('edit/{id_cauhoi}', ['uses' => 'CauHoiController@getEdit']);
+		Route::post('edit/{id_cauhoi}', ['uses' => 'CauHoiController@postEdit']);
+		Route::get('delete/{id_cauhoi}', ['uses' => 'CauHoiController@getDelete']);
+	});
+	Route::group(['prefix' => 'phuongan'], function(){
+		Route::get('list', ['uses' => 'AdminController@getListPhuongAn']);
 	});
 });
